3d1153552c715fd708cb8872c7bdfca9fcf33cbb
ilmamomin38-dot <ilmamomin38@gmail.com>
2026-07-01 14:23:02 +0530

    ilma

--- extra1/recap.py
+++
@@ -1,3 +0,0 @@
-student = {"name": "Riya", "age": 21, "course": "Python"}
-print(student["name"])
-print(student.get("city", "Not Available"))
--- extra1/sorting.py
+++
@@ -1,4 +0,0 @@
-marks = {"Riya": 88, "Aman": 95, "Sara": 72, "Kabir": 91}
-
-sorted_by_marks = dict(sorted(marks.items(), key=lambda item: item[1], reverse=True))
-print(sorted_by_marks)
